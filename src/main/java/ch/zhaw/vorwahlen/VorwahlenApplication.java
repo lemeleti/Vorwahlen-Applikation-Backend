d00287961609116f9fd722d1287a48e5ceffd590
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-09-28 09:13:38 +0200

    initial commit

---
+++ src/main/java/ch/zhaw/vorwahlen/VorwahlenApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.vorwahlen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VorwahlenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VorwahlenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/vorwahlen/VorwahlenApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.vorwahlen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VorwahlenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
