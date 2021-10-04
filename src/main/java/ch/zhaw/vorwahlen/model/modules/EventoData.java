1afbe1dbb9a4b0754bc2a24c407b70d96bd0c5b7
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-04 16:41:02 +0200

    add column definitions for evento data fields
    Co-Authored-By: meletlea <3164+meletlea@users.noreply.github.zhaw.ch>

--- src/main/java/ch/zhaw/vorwahlen/model/modules/EventoData.java
+++ src/main/java/ch/zhaw/vorwahlen/model/modules/EventoData.java
@@ -13,13 +13,16 @@ public class EventoData {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(columnDefinition = "text")
     private String shortDescription;
     private String coordinator;
     @Column(columnDefinition = "blob")
     private String learningObjectives;
     @Column(columnDefinition = "mediumblob")
     private String moduleContents;
+    @Column(columnDefinition = "text")
     private String literature;
+    @Column(columnDefinition = "text")
     private String suppLiterature;
     private String prerequisites;
     @Column(columnDefinition = "blob")
@@ -27,5 +30,6 @@ public class EventoData {
     private String language;
     @Column(columnDefinition = "blob")
     private String exams;
+    @Column(columnDefinition = "text")
     private String remarks;
 }
