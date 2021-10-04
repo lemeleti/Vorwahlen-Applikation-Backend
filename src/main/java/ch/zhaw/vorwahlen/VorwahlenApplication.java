e6e68b01b39e2af7f90d573e6f9b45cb7f871613
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-04 16:39:38 +0200

    remove comment and enable async

--- src/main/java/ch/zhaw/vorwahlen/VorwahlenApplication.java
+++ src/main/java/ch/zhaw/vorwahlen/VorwahlenApplication.java
@@ -2,11 +2,12 @@ package ch.zhaw.vorwahlen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class VorwahlenApplication{
 	public static void main(String[] args) {
-		//ConfigurableApplicationContext context =
 		SpringApplication.run(VorwahlenApplication.class, args);
 	}
 }
