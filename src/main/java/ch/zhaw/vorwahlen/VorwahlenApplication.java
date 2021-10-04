6d0a905d8407ed4e5f390d92d7a0e0baa0c78ace
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-04 10:34:37 +0200

    remove empty lines and comment

--- src/main/java/ch/zhaw/vorwahlen/VorwahlenApplication.java
+++ src/main/java/ch/zhaw/vorwahlen/VorwahlenApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VorwahlenApplication {
-
+public class VorwahlenApplication{
 	public static void main(String[] args) {
+		//ConfigurableApplicationContext context =
 		SpringApplication.run(VorwahlenApplication.class, args);
 	}
-
 }
