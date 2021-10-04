d5e769989d76cdd3a1760dae413b9d1128c1070c
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-04 10:40:21 +0200

    add module entity

---
+++ src/main/java/ch/zhaw/vorwahlen/model/modules/Module.java
@@ -0,0 +1,26 @@
+package ch.zhaw.vorwahlen.model.modules;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "modules")
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Module {
+    @Id
+    private String moduleNo;
+    private String shortModuleNo;
+    private int moduleId;
+    private String moduleGroup;
+    private boolean isIPModule;
+    private String institute;
+    private byte credits;
+    //Todo remove either from EventoData Class or here.
+    private String language;
+    // Todo add Durchführungssemester für VZ + TZ
+}
