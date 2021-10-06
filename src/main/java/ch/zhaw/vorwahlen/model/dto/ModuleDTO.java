603db90b9cc2ffe62670c24386f7b9543715e949
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-06 10:31:01 +0200

    Create ModuleDTO

---
+++ src/main/java/ch/zhaw/vorwahlen/model/dto/ModuleDTO.java
@@ -0,0 +1,15 @@
+package ch.zhaw.vorwahlen.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ModuleDTO {
+    @JsonProperty("module_no")
+    private String moduleNo;
+    @JsonProperty("module_title")
+    private String moduleTitle;
+    private String language;
+}
