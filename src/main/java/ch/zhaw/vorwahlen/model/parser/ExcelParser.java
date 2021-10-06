b58787d76dd1c587a5bde0dd1aa21f7f75addecd
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-06 09:34:03 +0200

    Add abstract ExcelParser class for future excel parsing

---
+++ src/main/java/ch/zhaw/vorwahlen/model/parser/ExcelParser.java
@@ -0,0 +1,57 @@
+package ch.zhaw.vorwahlen.model.parser;
+
+import ch.zhaw.vorwahlen.model.modules.Module;
+import ch.zhaw.vorwahlen.model.modules.ModuleStringTable;
+import ch.zhaw.vorwahlen.model.modules.StringTable;
+import lombok.AllArgsConstructor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@AllArgsConstructor
+public abstract class ExcelParser<T, S extends StringTable<?>> {
+    private final String fileLocation;
+    private final String workSheet;
+    private final Class<S> clazz;
+
+    public List<T> parseModulesFromXLSX() throws IOException {
+        List<T> moduleList = new ArrayList<>();
+        T object;
+
+        try (FileInputStream fis = new FileInputStream(fileLocation)) {
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            // todo somehow let the user decide which sheet should be parsed.
+            XSSFSheet moduleSheet = workbook.getSheet(workSheet);
+
+            Iterator<Row> rowIterator = moduleSheet.rowIterator();
+            if (rowIterator.hasNext())
+                setCellIndexes(rowIterator.next());
+
+            while (rowIterator.hasNext()) {
+                if ((object = createObjectFromRow(rowIterator.next())) != null)
+                    moduleList.add(object);
+            }
+
+        }
+        return moduleList;
+    }
+
+    void setCellIndexes(Row row) {
+        for (Cell cell : row) {
+            String cellValue = cell.getStringCellValue().trim().replace("\n", "");
+
+            S stringTable = StringTable.getConstantByValue(cellValue, clazz);
+            if (stringTable != null)
+                stringTable.setCellNumber(cell.getColumnIndex());
+        }
+    }
+
+    abstract T createObjectFromRow(Row row);
+}
