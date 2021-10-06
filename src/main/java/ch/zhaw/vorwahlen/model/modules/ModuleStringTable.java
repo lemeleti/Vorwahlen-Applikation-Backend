d33d7a89d5ac50350d8c2bc10b4718933668328f
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-06 09:33:24 +0200

    remove static HashMap and use StringTable interface instead

--- src/main/java/ch/zhaw/vorwahlen/model/modules/ModuleStringTable.java
+++ src/main/java/ch/zhaw/vorwahlen/model/modules/ModuleStringTable.java
@@ -5,13 +5,10 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @AllArgsConstructor
 @ToString
 // Todo search a better name
-public enum ModuleStringTable {
+public enum ModuleStringTable implements StringTable<ModuleStringTable> {
     NO("Modulkürzel", -1),
     SHORT_NO("Stammkürzel(Farbcode nach Modultafel)", -1),
     TITLE("Modulbezeichnung Deutsch(Farbcode nach Curriculum)", -1),
@@ -22,19 +19,8 @@ public enum ModuleStringTable {
     CREDITS("Credits/SWL", -1),
     LANGUAGE("Unterrichtssprache", -1);
 
-    private static final Map<String, ModuleStringTable> cellNameMap = new HashMap<>();
-
+    @Getter
     private final String cellHeaderName;
     @Setter @Getter
     private int cellNumber;
-
-    static {
-        for (ModuleStringTable moduleTranslator : ModuleStringTable.values()) {
-            cellNameMap.put(moduleTranslator.cellHeaderName, moduleTranslator);
-        }
-    }
-
-    public static ModuleStringTable getConstantByValue(String cellHeaderName) {
-        return cellNameMap.get(cellHeaderName);
-    }
 }
