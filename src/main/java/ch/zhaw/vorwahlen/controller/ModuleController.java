a1ab952108b918563a576a288a1365781cd24303
meletlea <meletlea@students.zhaw.ch>
2021-10-12 11:54:29 +0200

    add check on empty file for module list import request

--- src/main/java/ch/zhaw/vorwahlen/controller/ModuleController.java
+++ src/main/java/ch/zhaw/vorwahlen/controller/ModuleController.java
@@ -4,7 +4,11 @@ import ch.zhaw.vorwahlen.model.dto.ModuleDTO;
 import ch.zhaw.vorwahlen.service.ModuleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -34,6 +38,7 @@ public class ModuleController {
      */
     @PostMapping(path = {"/", ""})
     public ResponseEntity<String> saveModulesFromExcel(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) return ResponseEntity.notFound().build();
         moduleService.importModuleExcel(file);
         return ResponseEntity.ok().build();
     }
