fece45fec1d8cea5809b11c4cbcfd02c98819d2b
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-13 12:59:37 +0200

    replace * with single imports #2

--- src/main/java/ch/zhaw/vorwahlen/controller/ModuleController.java
+++ src/main/java/ch/zhaw/vorwahlen/controller/ModuleController.java
@@ -4,7 +4,11 @@ import ch.zhaw.vorwahlen.model.dto.ModuleDTO;
 import ch.zhaw.vorwahlen.service.ModuleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
