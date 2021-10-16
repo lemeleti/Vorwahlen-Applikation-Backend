4c49398b953b4c1f704c486d2a7cd2f9699493a9
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-16 18:02:51 +0200

    add continuous deployment settings

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,14 @@ docker build -t backend .
 docker tag backend vorwahlen/backend:latest
 docker push vorwahlen/backend
 
+echo SETTING UP SSH
+curl -sflL 'https://raw.githubusercontent.com/appveyor/secure-file/master/install.sh' | bash -e -
+./appveyor-tools/secure-file -decrypt ./pa.pem.enc -secret $SSH_ENC_PW -salt $SSH_ENC_SALT -out pa.pem
+chmod 0600 pa.pem
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+
+echo RUNNING DOCKER IMAGE UPDATE ON REMOTE SERVER
+ssh -i pa.pem ubuntu@vorwahlen.cloudlab.zhaw.ch "cd wpm; docker-compose pull && docker-compose up -d"
+
 echo "Image has been uploaded to Docker Hub."
 
