6a4651aba0d3fba9e041b524892ac58e4e224c8b
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-10-25 12:31:49 +0200

    implement user roles #27

--- src/main/java/ch/zhaw/vorwahlen/authentication/CustomAuthProvider.java
+++ src/main/java/ch/zhaw/vorwahlen/authentication/CustomAuthProvider.java
@@ -1,24 +1,46 @@
 package ch.zhaw.vorwahlen.authentication;
 
+import ch.zhaw.vorwahlen.model.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.session.SessionAuthenticationException;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Component
+@PropertySource("classpath:settings.properties")
 public class CustomAuthProvider implements AuthenticationProvider {
+    private final String[] admins;
+
+    public CustomAuthProvider(@Value("${admin}") String[] admins) {
+        this.admins = admins;
+    }
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         CustomAuthToken authToken = (CustomAuthToken) authentication;
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        User user;
 
         if (authToken.getShibbolethSession() == null || authToken.getShibbolethSession().isEmpty()) {
             throw new SessionAuthenticationException("Session could not be created");
         }
 
-        return new CustomAuthToken(new ArrayList<>(), authToken.getShibbolethSession(), authToken.getUser());
+        user = authToken.getUser();
+        for (String admin : admins) {
+            if (user.getMail().equals(admin)) {
+                authorities.add(new SimpleGrantedAuthority("ADMIN"));
+            }
+        }
+
+        return new CustomAuthToken(authorities, authToken.getShibbolethSession(), user);
     }
 
     @Override
--- src/main/java/ch/zhaw/vorwahlen/authentication/SecurityAdapter.java
+++ src/main/java/ch/zhaw/vorwahlen/authentication/SecurityAdapter.java
@@ -2,11 +2,13 @@ package ch.zhaw.vorwahlen.authentication;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 @Configuration
@@ -15,18 +17,22 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
     public static final String INIT_SESSION_URL = "https://vorwahlen.cloudlab.zhaw.ch/session/init";
     private final CustomAuthProvider customAuthProvider;
     private final CustomBasicAuthenticationEntryPoint authenticationEntryPoint;
+    private final String[] allowedPaths = {"/module**", "/", "/error**"};
+    private final String[] protectedPaths = {"/module**", "/dispensation**", "/class**", "/session/info"};
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(customAuthProvider);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, allowedPaths).permitAll()
+                .antMatchers(protectedPaths).hasAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and().httpBasic().authenticationEntryPoint(authenticationEntryPoint)
                 .and().addFilterBefore(new AuthFilter(), BasicAuthenticationFilter.class)
-                .csrf().disable();
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 }
