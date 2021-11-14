92a775475fa7c1f8ce841f711f3d7a74c23081e1
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-11-14 21:22:53 +0100

    replace controller with restcontroller annotation

--- src/main/java/ch/zhaw/vorwahlen/controller/ModuleElectionController.java
+++ src/main/java/ch/zhaw/vorwahlen/controller/ModuleElectionController.java
@@ -15,9 +15,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextImpl;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.transaction.Transactional;
 
@@ -25,7 +25,7 @@ import javax.transaction.Transactional;
  * Controller for a module.
  */
 @RequiredArgsConstructor
-@Controller
+@RestController
 @RequestMapping("election")
 public class ModuleElectionController {
 
