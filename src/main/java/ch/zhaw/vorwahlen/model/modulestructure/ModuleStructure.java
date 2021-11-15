46e84f58243e9a232b835311d12e8901994bedff
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-11-16 00:02:55 +0100

    create interface to describe module structure #41

---
+++ src/main/java/ch/zhaw/vorwahlen/model/modulestructure/ModuleStructure.java
@@ -0,0 +1,11 @@
+package ch.zhaw.vorwahlen.model.modulestructure;
+
+import java.util.Map;
+
+public interface ModuleStructure {
+    Map<Integer, Integer> contextModules();
+    Map<Integer, Integer> projectModule();
+    Map<Integer, Integer> interdisciplinaryModules();
+    Map<Integer, Integer> subjectModules();
+    Map<Integer, Integer> bachelorModule();
+}
