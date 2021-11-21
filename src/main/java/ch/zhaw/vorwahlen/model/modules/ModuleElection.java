7ce8a695beb5112708d023b1fdfdc90b0f02bc19
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-11-21 17:38:29 +0100

    generate module election id automatically

--- src/main/java/ch/zhaw/vorwahlen/model/modules/ModuleElection.java
+++ src/main/java/ch/zhaw/vorwahlen/model/modules/ModuleElection.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -20,6 +22,7 @@ import java.util.Set;
 public class ModuleElection {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @OneToOne
