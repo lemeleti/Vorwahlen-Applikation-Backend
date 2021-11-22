55ff0a375a502b3961a7a017fca80f035268caed
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-11-22 20:11:37 +0100

    remove unused import

--- src/main/java/ch/zhaw/vorwahlen/model/modulestructure/ModuleStructureGenerator.java
+++ src/main/java/ch/zhaw/vorwahlen/model/modulestructure/ModuleStructureGenerator.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Predicate;
 
 @RequiredArgsConstructor
