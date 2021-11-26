a9489d63d5f83ba568416fcc9cd39fb0621a0acf
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-11-26 18:12:16 +0100

    remove proxy bean and use scope prototype with proxy instead

--- src/main/java/ch/zhaw/vorwahlen/config/ElectionConfig.java
+++ src/main/java/ch/zhaw/vorwahlen/config/ElectionConfig.java
@@ -4,30 +4,26 @@ import ch.zhaw.vorwahlen.exporter.ExcelModuleElectionExporter;
 import ch.zhaw.vorwahlen.exporter.ModuleElectionExporter;
 import ch.zhaw.vorwahlen.model.modules.Student;
 import ch.zhaw.vorwahlen.model.modulestructure.ModuleDefinition;
-import ch.zhaw.vorwahlen.model.modulestructure.ModuleDefinitionFullTime;
-import ch.zhaw.vorwahlen.model.modulestructure.ModuleDefinitionPartTime;
+import ch.zhaw.vorwahlen.model.user.User;
 import ch.zhaw.vorwahlen.modulevalidation.ElectionValidator;
 import ch.zhaw.vorwahlen.modulevalidation.FullTimeElectionValidator;
 import ch.zhaw.vorwahlen.modulevalidation.PartTimeElectionValidator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-
-import java.util.function.Function;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @RequiredArgsConstructor
 public class ElectionConfig {
-    public static final String ELECTION_VALIDATOR="electionValidator";
-    public static final String MODULE_STRUCTURE="moduleStructure";
-    private final ModuleDefinitionFullTime structureFullTime;
-    private final ModuleDefinitionPartTime structurePartTime;
-
-    @Bean(name = ELECTION_VALIDATOR)
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public ElectionValidator electionValidator(Student student) {
+    @Bean
+    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public ElectionValidator electionValidator() {
+        var student = getStudentFromSecurityContext();
         ElectionValidator electionValidator = new FullTimeElectionValidator(student);
         if (student.isTZ()) {
             electionValidator = new PartTimeElectionValidator(student);
@@ -59,4 +55,8 @@ public class ElectionConfig {
         return new ExcelModuleElectionExporter();
     }
 
+    private Student getStudentFromSecurityContext() {
+        var user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return user.getStudent();
+    }
 }
