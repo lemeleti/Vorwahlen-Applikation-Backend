48a88230342fa150b53b5d873258fbce259958a1
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-11-27 15:20:46 +0100

    autowire electionSemester in election service class
    Co-Authored-By: meletlea <3164+meletlea@users.noreply.github.zhaw.ch>

--- src/main/java/ch/zhaw/vorwahlen/service/ElectionService.java
+++ src/main/java/ch/zhaw/vorwahlen/service/ElectionService.java
@@ -3,6 +3,7 @@ package ch.zhaw.vorwahlen.service;
 import ch.zhaw.vorwahlen.exporter.ModuleElectionExporter;
 import ch.zhaw.vorwahlen.model.dto.ElectionTransferDTO;
 import ch.zhaw.vorwahlen.model.dto.ModuleElectionDTO;
+import ch.zhaw.vorwahlen.model.modules.ElectionSemesters;
 import ch.zhaw.vorwahlen.model.modules.ModuleElection;
 import ch.zhaw.vorwahlen.model.modules.Student;
 import ch.zhaw.vorwahlen.model.modulestructure.ModuleDefinition;
@@ -11,7 +12,6 @@ import ch.zhaw.vorwahlen.model.modulestructure.ModuleStructureGenerator;
 import ch.zhaw.vorwahlen.modulevalidation.ElectionValidator;
 import ch.zhaw.vorwahlen.repository.ElectionRepository;
 import ch.zhaw.vorwahlen.repository.ModuleRepository;
-import ch.zhaw.vorwahlen.repository.ValidationSettingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
@@ -29,6 +29,7 @@ public class ElectionService {
     private final ElectionValidator electionValidator;
     private final ModuleDefinition moduleDefinition;
     private final ModuleElectionExporter exporter;
+    private final ElectionSemesters electionSemesters;
 
     /**
      * Get election data for the specified user.
@@ -89,7 +90,7 @@ public class ElectionService {
     private ElectionTransferDTO createElectionTransferDTO(Student student,
                                                           ModuleElection moduleElection, boolean saved) {
         var electionStructure =
-                new ModuleStructureGenerator(moduleDefinition, student, moduleElection).generateStructure();
+                new ModuleStructureGenerator(moduleDefinition, student, moduleElection, electionSemesters).generateStructure();
         return new ElectionTransferDTO(electionStructure, saved, moduleElection.isElectionValid());
     }
 
