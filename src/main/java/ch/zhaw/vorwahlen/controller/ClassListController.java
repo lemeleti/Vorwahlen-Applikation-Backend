e13122881aaa7273c1c7581cce5ad3030ec23802
Carl Lubojanski <carl.lubojanski@outlook.com>
2021-12-01 10:39:35 +0100

    replace request param with path variable

--- src/main/java/ch/zhaw/vorwahlen/controller/ClassListController.java
+++ src/main/java/ch/zhaw/vorwahlen/controller/ClassListController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,8 +50,8 @@ public class ClassListController {
         return ResponseEntity.ok().build();
     }
 
-    @PatchMapping(path = "/{studentid}")
-    public ResponseEntity<Void> updateValidationSettings(@RequestParam("studentid") String studentId, @AuthenticationPrincipal User user,
+    @PatchMapping(path = "/{studentId}")
+    public ResponseEntity<Void> updateValidationSettings(@PathVariable String studentId, @AuthenticationPrincipal User user,
                                                          @RequestBody ValidationSettingDTO validationSettingDTO) {
         // todo: patch only changes not everything
         if(studentId.equals(user.getMail())) {
