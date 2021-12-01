e2a570a2e322a4d97bb928766e8b507179c717bf
meletlea <meletlea@students.zhaw.ch>
2021-12-01 17:18:03 +0100

    fix CORSAdvice move errors

--- src/main/java/ch/zhaw/vorwahlen/config/WebSocketConfig.java
+++ src/main/java/ch/zhaw/vorwahlen/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package ch.zhaw.vorwahlen.config;
 
+import ch.zhaw.vorwahlen.security.config.CORSAdvice;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
