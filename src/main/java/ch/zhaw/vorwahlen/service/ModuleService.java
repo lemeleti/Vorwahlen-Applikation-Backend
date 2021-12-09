460e50980de8164d0f9eda463deb28378f55d0d8
meletlea <meletlea@students.zhaw.ch>
2021-12-09 21:25:01 +0100

    add missing import lost in merge conflict

--- src/main/java/ch/zhaw/vorwahlen/service/ModuleService.java
+++ src/main/java/ch/zhaw/vorwahlen/service/ModuleService.java
@@ -23,6 +23,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
